5db08dca66253f6ed1bba315bec96dd2db98a34a
Max Ehrlich <max.ehr@gmail.com>
2014-07-22 17:25:32 -0400

    Fixed errors in require config

--- client/source/main.js
+++ client/source/main.js
@@ -1,13 +1,13 @@
 require.config({
         baseUrl: './',
         paths: {
-            jquery: '../bower_components/jquery/dist/jquery',
-            knockout: '../bower_components/knockout-3.1.0/index',
-            requirejs: '../bower_components/',
-            domready: '../bower_components/domready/ready',
-            bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
-            crossroads: '../bower_components/crosroads.js/dist/crossroads.min',
-            application: '../bower_components/application/Application'
+            jquery: 'bower_components/jquery/dist/jquery',
+            knockout: 'bower_components/knockout-3.1.0/index',
+            requirejs: 'bower_components/requirejs/require',
+            domready: 'bower_components/domready/ready',
+            bootstrap: 'bower_components/bootstrap/dist/js/bootstrap',
+            crossroads: 'bower_components/crosroads.js/dist/crossroads.min',
+            application: 'bower_components/application/application'
         },
         shim: {
             'boostrap': {
