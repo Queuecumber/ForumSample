2aefcca13400265ed1a5bbbb4ee0a1a78c16e4b2
Max Ehrlich <max.ehr@gmail.com>
2014-09-06 11:06:54 -0400

    Slight refactoring

--- client/source/components/post/post.js
+++ client/source/components/post/post.js
@@ -2,9 +2,11 @@ define(['knockout', 'application'], function (ko, application)
 {
     return function ()
     {
+        var self = this;
+
         self.post = ko.observable();
 
-        self.activated.on(function (event, p)
+        self.activated.on(function (e, p)
         {
             self.post(p);
         });
