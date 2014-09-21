a77a9dbcb0954125d92920b10846819fd8cf3409
Max Ehrlich <max.ehr@gmail.com>
2014-09-21 10:10:36 -0400

    Added constructor and server connection

--- client/source/model/thread.js
+++ client/source/model/thread.js
@@ -1,12 +1,50 @@
-define(['knockout'], function (ko)
+define(['knockout', 'socketio'], function (ko, io)
 {
-    var thread = function ()
+    var thread = function (threadModel)
     {
-        this.creator = ko.observable();
+        // data
+        this.id = threadModel.id;
 
-        this.title = ko.observable('');
+        this.creator = threadModel.user;
+
+        this.title = ko.observable(threadModel.title);
 
         this.posts = ko.observableArray([]);
+
+        // server mutations
+        var socket = io();
+        socket.emit('join', 'thread:' + id);
+
+        socket.on('thread:' + id + ':updated', function (updatedThread)
+        {
+            this.title(updatedThread.title);
+        }.bind(this));
+
+        // client mutations
+        this.title.subscribe(function (newTitle)
+        {
+            socket.emit('downstream', {
+                channel: 'thread:' + id + ':updated',
+                data: this.serialize()
+            });
+        }.bind(this));
+
+        // TODO fill in post add/remove mutations
+
+        // serialize
+        this.serialize = function ()
+        {
+            var pure = ko.toJS(this);
+            delete pure.posts;
+
+            return pure;
+        };
+
+        // dispose
+        this.dispose = function ()
+        {
+            socket.emit('leave', 'thread:' + id);
+        };
     };
 
     return thread;
