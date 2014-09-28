4b3d1b53e42b4af0620e006f7ab5c9acc8f37213
Max Ehrlich <max.ehr@gmail.com>
2014-09-28 15:23:13 -0400

    Added socket.io connection

--- client/source/model/model.js
+++ client/source/model/model.js
@@ -1,7 +1,14 @@
-define(['knockout'], function (ko)
+define(['knockout', 'socketio'], function (ko, io)
 {
+    var socket = io();
+
     var model = {
-        boards: ko.observableArray([])
+        boards: ko.observableArray([]),
+
+        sync: function ()
+        {
+            socket.emit('join', 'global:-1');
+        }
     };
 
     return model;
