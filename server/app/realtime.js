c9e77b4707a6d1dad94108c089fb2452409dee73
Max Ehrlich <max.ehr@gmail.com>
2014-09-28 15:24:13 -0400

    Added downstream updates, better logging, and cleaner socket.io connection

--- server/app/realtime.js
+++ server/app/realtime.js
@@ -1,5 +1,5 @@
 var config = require('config');
-var SocketIO = require('socket.io');
+var socketIO = require('socket.io');
 var httpServer = require('http').Server;
 var model = require('../model');
 var updater = require('redis').createClient(config.redis.port, config.redis.host);
@@ -8,8 +8,7 @@ var updater = require('redis').createClient(config.redis.port, config.redis.host
 var realtime = {
     start: function (server)
     {
-        var srv = httpServer(server);
-        realtime.io = new SocketIO(srv);
+        realtime.io = socketIO.listen(server);
 
         realtime.io.on('connection', realtime.initiate);
 
@@ -17,22 +16,26 @@ var realtime = {
 
         updater.on('message', function (channel, message)
         {
+            process.log.info(channel, 'reported upstream change %j', message);
+
             var room = channel.substring(0, channel.lastIndexOf(':'));
             realtime.io.to(room).emit(channel, message);
         });
-
-
     },
 
     initiate: function (socket)
     {
+        process.log.info('New client connected from %s', socket.handshake.address);
+
         socket.on('join', function (endpoint)
         {
+            process.log.info('Client %s reported interest in %s', socket.handshake.address, endpoint);
             socket.join(endpoint);
         });
 
         socket.on('leave', function (endpoint)
         {
+            process.log.info('Client %s no longer interested in %s', socket.handshake.address, endpoint);
             socket.leave(endpoint);
         });
 
@@ -40,9 +43,46 @@ var realtime = {
         {
             // Do the actual model update
             // update: { channel: 'thread:145:add-post', data: '{post}' }
+            // { channel: thread:145:update, property: title, data: '{title} }
+
+            process.log.info(socket.handshake.address, 'reported downstream change %j', update);
+
+            var channel = update.channel.split(':');
+
+            var collection = channel[0];
+            var id = channel[1];
+            var event = channel[2];
+
+            realtime.applyDownstream(collection, id, event, update);
         });
 
-        process.log.info('New client connected');
+        socket.on('disconnect', function ()
+        {
+            process.log.info('Client %s disconnected', socket.handshake.address);
+        });
+    },
+
+    applyDownstream: function (collection, id, event, update)
+    {
+        switch (event)
+        {
+            case 'update':
+                model[collection]
+                    .find({ where: { id: id }})
+                    .then(function (instance)
+                    {
+                        process.log.info('Applying downstream update (%s:%d).%s=%s', collection, id, update.property, update.data);
+
+                        instance[update.property] = update.data;
+                        instance.save([update.property]);
+                    })
+                    .catch(function(err)
+                    {
+                        process.log.error('Error applying downstream update (%s:%d).%s=%s for %s: %s',
+                        collection, id, update.property, update.data, socket.handshake.address, err);
+                    });
+                break;
+        }
     }
 };
 
