4ddbc3aab8713ca25a4daec0c64f9051108df913
Max Ehrlich <max.ehr@gmail.com>
2014-10-04 12:31:16 -0400

    Coupled top level boards

--- client/source/model/model.js
+++ client/source/model/model.js
@@ -2,12 +2,22 @@ define(['knockout', 'socketio'], function (ko, io)
 {
     var socket = io();
 
+    socket
+
     var model = {
-        boards: ko.observableArray([]),
+        boards: ko.observableArray([]).extend({coupling: {
+            socket: socket,
+            channel: 'global:-1',
+            delta: {
+                added: ':board-added',
+                removed: ':board-removed'
+            }
+        }}),
 
         sync: function ()
         {
             socket.emit('join', 'global:-1');
+            socket.emit('sync', 'global:-1');
         }
     };
 
